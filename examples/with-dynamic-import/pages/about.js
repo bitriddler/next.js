a25094bb145ed4bdd00dc4d2bcedb8dbf59e1711
Kareem Mohamed <kareem3d.a@gmail.com>
2019-01-15 19:09:23 +0100

    Reproducing dynamic import bug

--- examples/with-dynamic-import/pages/about.js
+++ examples/with-dynamic-import/pages/about.js
@@ -1,11 +1,14 @@
+import React from 'react'
 import Header from '../components/Header'
 import Counter from '../components/Counter'
+import Hello2 from '../components/hello1'
 
 const About = () => (
   <div>
     <Header />
     <p>This is the about page.</p>
     <Counter />
+    <Hello2 />
   </div>
 )
 
